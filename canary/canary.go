3c006f4b406ac0121d6ed3a9fb20264c0972d650
dllewellyn <danieln.llewellyn@gmail.com>
2023-09-10 10:54:18 +0100

    Adding V1 of a canary checker

---
+++ canary/canary.go
@@ -0,0 +1,45 @@
+// Package canary provides utilities for canary detection.
+package canary
+
+import (
+	"errors"
+)
+
+// Mode represents the type of canary checking to be performed.
+type Mode int
+
+// Enumeration of available Modes.
+const (
+	Basic        Mode = iota // Basic mode performs an exact match check.
+	Intermediate             // Intermediate mode is not implemented yet.
+	Advanced                 // Advanced mode is not implemented yet.
+)
+
+// Result holds the result of a canary check.
+type Result struct {
+	Detected bool // Detected indicates if the canary was detected.
+}
+
+// Checker encapsulates the Mode and Canary string for performing canary checks.
+type Checker struct {
+	Mode   Mode   // Mode represents the canary checking mode.
+	Canary string // Canary holds the canary string to be checked.
+}
+
+// New creates a new instance of Checker.
+// It takes a Mode and a canary string as parameters.
+func New(mode Mode, canary string) *Checker {
+	return &Checker{Mode: mode, Canary: canary}
+}
+
+// Check performs a canary check based on the mode.
+// It returns a Result and an error.
+// The only implemented Mode as of now is Basic, which performs an exact match check.
+func (c *Checker) Check(output string) (Result, error) {
+	switch c.Mode {
+	case Basic:
+		return Result{Detected: output == c.Canary}, nil
+	default:
+		return Result{}, errors.New("Mode not implemented")
+	}
+}
---
+++ canary/canary_test.go
@@ -0,0 +1,41 @@
+package canary
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	checker := New(Basic, "test")
+	if checker.Mode != Basic || checker.Canary != "test" {
+		t.Errorf("New(Basic, \"test\") = %v; want Mode=Basic and Canary=test", checker)
+	}
+}
+
+func TestCheckBasic(t *testing.T) {
+	checker := New(Basic, "testCanary")
+
+	result, err := checker.Check("testCanary")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !result.Detected {
+		t.Errorf("Canary not detected, got: %v, want: true", result.Detected)
+	}
+
+	result, err = checker.Check("noMatch")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if result.Detected {
+		t.Errorf("Canary detected, got: %v, want: false", result.Detected)
+	}
+}
+
+func TestCheckUnsupportedMode(t *testing.T) {
+	checker := New(Intermediate, "testCanary")
+
+	_, err := checker.Check("testCanary")
+	if err == nil || err.Error() != "Mode not implemented" {
+		t.Errorf("Unexpected error, got: %v, want: Mode not implemented", err)
+	}
+}
