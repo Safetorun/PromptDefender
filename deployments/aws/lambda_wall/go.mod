922d99908c6b7132166aeb4c9220ff272dd2d382
dllewellyn <danieln.llewellyn@gmail.com>
2023-10-27 07:58:44 +0100

    Generifying the lambda deployment

--- deployments/aws/lambda_wall/go.mod
+++ deployments/aws/lambda_wall/go.mod
@@ -11,5 +11,6 @@ replace github.com/safetorun/PromptDefender/wall => ../../../wall
 require (
 	github.com/aws/aws-lambda-go v1.41.0
 	github.com/safetorun/PromptDefender/aiprompt v0.0.0-20231017173944-0a5da2b1ee56
+	github.com/safetorun/PromptDefender/aws/base_aws v0.0.0-00010101000000-000000000000
 	github.com/safetorun/PromptDefender/wall v0.0.0-20231017173944-0a5da2b1ee56
 )
--- deployments/aws/lambda_wall/lambda.go
+++ deployments/aws/lambda_wall/lambda.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/safetorun/PromptDefender/aiprompt"
+	"github.com/safetorun/PromptDefender/aws/base_aws"
 	"github.com/safetorun/PromptDefender/wall"
 	"os"
 )
+
+type WallLambda struct {
+	wallInstance *wall.Wall
+}
+
+func (w *WallLambda) Handle(promptRequest WallRequest) (*WallResponse, error) {
+	answer, err := w.wallInstance.CheckWall(wall.PromptToCheck{Prompt: promptRequest.Prompt})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &WallResponse{InjectionScore: &answer.Score}, nil
+}
 
 func Handler(_ context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	openAIKey, exists := os.LookupEnv("open_ai_api_key")
@@ -18,33 +32,17 @@ func Handler(_ context.Context, request events.APIGatewayProxyRequest) (events.A
 		return events.APIGatewayProxyResponse{StatusCode: 400}, fmt.Errorf("error retrieving API key: environment variable not set")
 	}
 
-	var promptRequest WallRequest
-
-	if err := json.Unmarshal([]byte(request.Body), &promptRequest); err != nil {
-		fmt.Printf("error unmarshalling request: %v\n", err)
-		return events.APIGatewayProxyResponse{StatusCode: 400}, err
-	}
-
-	fmt.Printf("Received request for %v\n", promptRequest)
-
 	wallBuilder := wall.New(aiprompt.NewOpenAI(openAIKey))
-	answer, err := wallBuilder.CheckWall(wall.PromptToCheck{Prompt: promptRequest.Prompt})
 
-	if err != nil {
-		fmt.Printf("error processing AI: %v\n", err)
-		return events.APIGatewayProxyResponse{StatusCode: 400}, fmt.Errorf("error processing AI request: %v", err)
-	}
+	handler := WallLambda{wallInstance: &wallBuilder}
 
-	response := WallResponse{InjectionScore: &answer.Score}
+	response, err := base_aws.BaseHandler[WallRequest, WallResponse](request, &handler)
 
-	jsonBytes, err := json.Marshal(response)
 	if err != nil {
 		return events.APIGatewayProxyResponse{StatusCode: 400}, err
 	}
 
-	jsonString := string(jsonBytes)
-
-	return events.APIGatewayProxyResponse{StatusCode: 200, Body: jsonString}, nil
+	return response, nil
 }
 
 func main() {
