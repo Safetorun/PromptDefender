31726dcf68d9aa4dc3c7042610ccf7ec11d285e0
dllewellyn <danieln.llewellyn@gmail.com>
2024-02-27 07:24:12 +0000

    Adding fixes to test

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -6,11 +6,18 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "api" {
-  depends_on = [aws_api_gateway_rest_api.api]
+  depends_on  = [aws_api_gateway_rest_api.api]
   rest_api_id = aws_api_gateway_rest_api.api.id
+  triggers    = {
+    redeployment = sha256(jsonencode(aws_api_gateway_rest_api.api.body))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_cloudwatch_log_group" "api_logs" { #tfsec:ignore:aws-cloudwatch-log-group-customer-key
+resource "aws_cloudwatch_log_group" "api_logs" {
+  #tfsec:ignore:aws-cloudwatch-log-group-customer-key
   name              = "/aws/api_gateway/${aws_api_gateway_rest_api.api.name}"
   retention_in_days = 14
 }
@@ -61,3 +68,4 @@ resource "local_file" "built_open_api_spec" {
     lambda_user_arn = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.aws_lambda_user.arn}/invocations",
   })
 }
+
