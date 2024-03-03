0c95b2cfb29674e341bcbd9646829b15c5a6ed66
dllewellyn <danieln.llewellyn@gmail.com>
2024-03-03 07:56:36 +0000

    Fixing recurring 502 errors on wall (#45)
    * Fixing recurring 502 errors on wall

---
+++ internal/wall/utils.go
@@ -0,0 +1,32 @@
+package wall
+
+import (
+	"log"
+	"time"
+)
+
+type RetryableFunc func() (*float64, error)
+
+func Retry(attempts int, sleep time.Duration, fn RetryableFunc) (*float64, error) {
+	returnVal, err := fn()
+
+	if err != nil {
+		if s, ok := err.(stop); ok {
+			return nil, s.error
+		}
+
+		if attempts--; attempts > 0 {
+			// Add some sleep here if you want to prevent spamming retries
+			time.Sleep(sleep)
+			log.Default().Println("Retrying after error: ", err, " Attempts left: ", attempts)
+			return Retry(attempts, 2*sleep, fn)
+		}
+
+		return nil, err
+	}
+	return returnVal, nil
+}
+
+type stop struct {
+	error
+}
