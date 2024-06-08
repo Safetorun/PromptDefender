124bb59b6cbae4fef711e7e5c3972fa7be642da0
dllewellyn <danieln.llewellyn@gmail.com>
2024-06-08 07:46:30 +0100

    Adding post request logging lambda

--- cmd/lambda_keep_py/shared.py
+++ cmd/lambda_keep_py/shared.py
@@ -13,7 +13,7 @@ from cache.cache import retrieve_item_if_exists, store_item
 logger = Logger(service="PromptDefender-Keep")
 
 
-def __retrieve_item_if_exists__(key):
+def __retrieve_item_if_exist(key):
     cache_table_name = os.getenv('CACHE_TABLE_NAME')
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(cache_table_name)
@@ -26,7 +26,7 @@ def __retrieve_item_if_exists__(key):
         return None
 
 
-def __store_item__(key, item):
+def __store_item(key, item):
     cache_table_name = os.getenv('CACHE_TABLE_NAME')
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(cache_table_name)
@@ -42,7 +42,7 @@ def cachable_result(
 ) -> dict:
     start_time = time.time()
 
-    return_data = retrieve_item_if_exists(event['body'], retrieve_function=__retrieve_item_if_exists__)
+    return_data = retrieve_item_if_exists(event['body'], retrieve_function=__retrieve_item_if_exist)
 
     if return_data is not None:
         logger.info("Retrieved from cache", return_data=return_data)
@@ -53,7 +53,7 @@ def cachable_result(
 
     logger.info("Storing in cache {}", result)
 
-    store_item(event['body'], result['body'], store_function=__store_item__)
+    store_item(event['body'], result['body'], store_function=__store_item)
 
     queue_message = to_request_log(event)
     queue_message["Response"] = result["body"]
