7ef5455750560816eb8670fc1199c2808d232de9
dllewellyn <danieln.llewellyn@gmail.com>
2024-06-18 07:51:31 +0100

    Wip removing golang and replacing with python for wall.

--- scripts/build_openai_client.sh
+++ scripts/build_openai_client.sh
@@ -5,5 +5,3 @@ runtime=$(python --version 2>&1 | cut -d ' ' -f 2 | cut -d '.' -f 2)
 
 # Create a new directory for the Python site-packages
 mkdir -p cmd/deps/cmd/lambda_wall_py/dist/python/lib/python3.${runtime}/site-packages/prompt_defender_client || exit 1
-
-cp -r cmd/client/prompt_defender_client/* cmd/deps/cmd/lambda_wall_py/dist/python/lib/python3.${runtime}/site-packages/prompt_defender_client || exit 1
